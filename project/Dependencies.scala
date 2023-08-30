95a312c99922de55cf3fdb5ca9f31070bca4e3cb
Marcus Girolneto <marcus.girolneto@gmail.com>
2023-08-30 13:12:56 -0300

    Updated version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,8 @@ import sbt._
 object Dependencies {
 
   object V {
-    val tessellation = "2.0.0-alpha.13"
+    //Default version of tessellation develop branch
+    val tessellation = "2.0.0-SNAPSHOT"
     val decline = "2.4.1"
   }
   def tessellation(artifact: String): ModuleID = "org.constellation" %% s"tessellation-$artifact" % V.tessellation
